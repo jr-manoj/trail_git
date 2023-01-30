bf8d80149085a18657634b1c67bfebec4438dcf9
jr-manoj <mpsandip12@gmail.com>
2023-01-30 08:33:33 +0545

    update

--- Promises/main.js
+++ Promises/main.js
@@ -1,17 +1,17 @@
 // Syntax to create promise (which is asynchnorous in nature)
 
-const promise = new Promise((resolve, reject) => {  // two parameters, resolve and reject.
-    // As login example.
+// const promise = new Promise((resolve, reject) => {  // two parameters, resolve and reject.
+//     // As login example.
 
-    const sum = 1+ 1;
-    if (sum ===2){
-        resolve();
-    }
-    else{
-        reject();
-    }
+//     const sum = 1+ 1;
+//     if (sum ===2){
+//         resolve();
+//     }
+//     else{
+//         reject();
+//     }
 
-});
+// });
 
 
 // promise.then(() => ){  --> If resolve then run this
@@ -19,14 +19,64 @@ const promise = new Promise((resolve, reject) => {  // two parameters, resolve a
 // }
 // promise.catch()   -> If reject run this
 
-promise.then(() => {  // We can add then function as more we want.
-    console.log("This is resolve.")
+// promise.then(() => {  // We can add then function as more we want.
+//     console.log("This is resolve.")
 
-}).then(() => {
-    console.log("THe final value must be updated to 10.")
+// }).then(() => {
+//     console.log("THe final value must be updated to 10.")
+// });
+
+
+// promise.catch(() => {
+//     console.log("This is reject.")
+// });
+
+const promise_1 = new Promise((resolve, reject)=>{
+
+    console.log("Go and bring some tacos")
+    const tacos = true;
+
+    if (tacos)
+        resolve("Tacos was found...")
+    else
+        reject("Alas, no tacos tonight.")
+
+
 });
 
-
-promise.catch(() => {
-    console.log("This is reject.")
+const tacosFound = ((result)=>{
+    console.log(result);
+    console.log("Cook up some pasta.")
 });
+
+const tacosNotFound = ((result)=>{
+    console.log(result);
+    console.log("Cook up some pasta.")
+});
+
+promise_1.then(tacosFound).catch(tacosNotFound);
+
+
+
+
+
+
+// promise_1.catch((result)=>{
+    
+//     console.log(result);
+
+//     console.log("Cook up some pasta.")
+// });
+
+// promise_1.then((result)=>{
+//     console.log(reject)
+// });
+
+
+
+// promise_1.catch((result)=>{
+    
+//     console.log(result);
+
+//     console.log("Cook up some pasta.")
+// });
